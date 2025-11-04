1974580760e3994be203e710c30a18c550d8e977
jetyue04 <jetyue04@gmail.com>
2025-11-04 02:44:19 -0800

    more sanity checks

--- src/model_minimal.py
+++ src/model_minimal.py
@@ -47,7 +47,7 @@ class CausalSelfAttention(nn.Module):
     explicit implementation here to show that there is nothing too scary here.
     """
 
-    def __init__(self, config, return_att=False, mask_input_loss=True):
+    def __init__(self, config, return_att=False):
         super().__init__()
         assert config.n_embd % config.n_head == 0
 
@@ -93,8 +93,7 @@ class CausalSelfAttention(nn.Module):
 
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, 0.0)
         
         att_copy = att.clone().detach()
 
@@ -120,17 +119,17 @@ class Block(nn.Module):
         self.ln_1 = nn.LayerNorm(config.n_embd)
         self.attn = CausalSelfAttention(config, return_att=return_att)
         self.ln_2 = nn.LayerNorm(config.n_embd)
-        self.mlp = nn.ModuleDict(
-            dict(
-                c_fc=nn.Linear(config.n_embd, 4 * config.n_embd),
-                c_proj=nn.Linear(4 * config.n_embd, config.n_embd),
-                act=NewGELU(),
-                # dropout=nn.Dropout(config.resid_pdrop),
-            )
-        )
+        # self.mlp = nn.ModuleDict(
+        #     dict(
+        #         c_fc=nn.Linear(config.n_embd, 4 * config.n_embd),
+        #         c_proj=nn.Linear(4 * config.n_embd, config.n_embd),
+        #         act=NewGELU(),
+        #         # dropout=nn.Dropout(config.resid_pdrop),
+        #     )
+        # )
         
-        m = self.mlp
-        self.mlpf = lambda x: m.c_proj(m.act(m.c_fc(x)))  # MLP forward
+        # m = self.mlp
+        # self.mlpf = lambda x: m.c_proj(m.act(m.c_fc(x)))  # MLP forward
         self.return_att = return_att
 
     def forward(self, x):
@@ -138,26 +137,25 @@ class Block(nn.Module):
             x_prev, att = self.attn(self.ln_1(x))
             x = x + x_prev
             
-            x = x + self.mlpf(self.ln_2(x))
+            # x = x + self.mlpf(self.ln_2(x))
             
             return x, att
 
         else:
             x = x + self.attn(self.ln_1(x))
-            x = x + self.mlpf(self.ln_2(x))
+            # x = x + self.mlpf(self.ln_2(x))
             return x
 
 
-class GPTSoftmax(nn.Module):
+class GPTLinear(nn.Module):
     """GPT Language Model"""
 
-    def __init__(self, config, return_att=False, mask_input_loss=True):
+    def __init__(self, config, return_att=False):
         super().__init__()
         assert config.vocab_size is not None
         assert config.block_size is not None
         self.block_size = config.block_size
         self.return_att = return_att
-        self.mask_input_loss = mask_input_loss
 
         self.transformer = nn.ModuleDict(
             dict(
@@ -173,7 +171,186 @@ class GPTSoftmax(nn.Module):
             )
         )
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        # init all weights, and apply a special scaled init to the residual projections, per GPT-2 paper
+        self.apply(self._init_weights)
+        for pn, p in self.named_parameters():
+            if pn.endswith("c_proj.weight"):
+                torch.nn.init.normal_(
+                    p, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layer)
+                )
+
+        # report number of parameters (note we don't count the decoder parameters in lm_head)
+        n_params = sum(p.numel() for p in self.transformer.parameters())
+        # print("number of parameters: %.2fM" % (n_params / 1e6,))
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        elif isinstance(module, nn.LayerNorm):
+            torch.nn.init.zeros_(module.bias)
+            torch.nn.init.ones_(module.weight)
+
+
+    # Only used for weight decay experiments -------------------------------------------
+    def configure_optimizers(self, train_config):
+        """
+        This long function is unfortunately doing something very simple and is being very defensive:
+        We are separating out all parameters of the model into two buckets: those that will experience
+        weight decay for regularization and those that won't (biases, and layernorm/embedding weights).
+        We are then returning the PyTorch optimizer object.
+        """
+
+        # separate out all parameters to those that will and won't experience regularizing weight decay
+        decay = set()
+        no_decay = set()
+        whitelist_weight_modules = (torch.nn.Linear, )
+        blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
+        for mn, m in self.named_modules():
+            for pn, p in m.named_parameters():
+                fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
+                # random note: because named_modules and named_parameters are recursive
+                # we will see the same tensors p many many times. but doing it this way
+                # allows us to know which parent module any tensor p belongs to...
+                if pn.endswith('bias'):
+                    # all biases will not be decayed
+                    no_decay.add(fpn)
+                elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
+                    # weights of whitelist modules will be weight decayed
+                    decay.add(fpn)
+                elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
+                    # weights of blacklist modules will NOT be weight decayed
+                    no_decay.add(fpn)
+
+        # validate that we considered every parameter
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        inter_params = decay & no_decay
+        union_params = decay | no_decay
+        assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
+        assert len(param_dict.keys() - union_params) == 0, "parameters %s were not separated into either decay/no_decay set!" \
+                                                    % (str(param_dict.keys() - union_params), )
+
+        # create the pytorch optimizer object
+        optim_groups = [
+            {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": train_config.wd},
+            {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
+        ]
+        optimizer = torch.optim.AdamW(optim_groups, lr=train_config.lr)
+        return optimizer
+    # -------------------------------------------
+
+
+    def forward(self, idx, prompt_len, targets=None):
+        device = idx.device
+        b, t = idx.size()
+        assert (
+            t <= self.block_size
+        ), f"Cannot forward sequence of length {t}, block size is only {self.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(
+            0
+        )  # shape (1, t)
+
+        # forward the GPT model itself
+        tok_emb = self.transformer.wte(idx)  # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.transformer.wpe(pos)  # position embeddings of shape (1, t, n_embd)
+        x = tok_emb + pos_emb
         
+        for block in self.transformer.h:
+            if self.return_att:
+                x, attn_map = block(x)
+            else:
+                x = block(x)
+
+        
+        x = self.transformer.ln_f(x)
+        
+        # Track residual state before LM head for representation collapse
+        pre_lm_h = x.clone().detach()
+        
+        # Final logits
+        logits = self.lm_head(x)
+
+        # if we are given some desired targets also calculate the loss
+        loss = None
+
+        if targets is not None:
+            # Masking input tokens
+            targets_masked = targets.clone()
+            targets_masked[:, :prompt_len - 1] = -1
+            loss = F.cross_entropy(
+                logits.reshape(-1, logits.size(-1)),
+                targets_masked.reshape(-1),
+                ignore_index=-1,
+            )
+        
+        if self.return_att:
+            return attn_map, pre_lm_h, logits, loss
+        
+        return pre_lm_h, logits, loss
+    
+    @torch.no_grad()
+    def generate(
+        self, idx, max_new_tokens, prompt_len, temperature=1.0, do_sample=False, top_k=None
+    ):
+        """
+        Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
+        the sequence max_new_tokens times, feeding the predictions back into the model each time.
+        Most likely you'll want to make sure to be in model.eval() mode of operation for this.
+        """
+        
+        for _ in range(max_new_tokens):
+            # if the sequence context is growing too long we must crop it at block_size
+            idx_cond = (
+                idx if idx.size(1) <= self.block_size else idx[:, -self.block_size :]
+            )
+            # forward the model to get the logits for the index in the sequence
+            _, _, logits, _ = self(idx_cond, prompt_len)
+            # pluck the logits at the final step and scale by desired temperature
+            logits = logits[:, -1, :] / temperature
+            # optionally crop the logits to only the top k options
+            if top_k is not None:
+                v, _ = torch.topk(logits, top_k)
+                logits[logits < v[:, [-1]]] = -float("Inf")
+            # apply softmax to convert logits to (normalized) probabilities
+            probs = F.softmax(logits, dim=-1)
+            # either sample from the distribution or take the most likely element
+            if do_sample:
+                idx_next = torch.multinomial(probs, num_samples=1)
+            else:
+                _, idx_next = torch.topk(probs, k=1, dim=-1)
+            # append sampled index to the running sequence and continue
+            idx = torch.cat((idx, idx_next), dim=1)
+
+        return idx
+
+class GPTSoftmax(nn.Module):
+    """GPT Language Model"""
+
+    def __init__(self, config, return_att=False):
+        super().__init__()
+        assert config.vocab_size is not None
+        assert config.block_size is not None
+        self.block_size = config.block_size
+        self.return_att = return_att
+
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(config.vocab_size, config.n_embd),
+                wpe=nn.Embedding(config.block_size, config.n_embd),
+                h=nn.ModuleList(
+                    [
+                        Block(config, return_att=self.return_att)
+                        for _ in range(config.n_layer)
+                    ]
+                ),
+                ln_f=nn.LayerNorm(config.n_embd),
+            )
+        )
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
         # init all weights, and apply a special scaled init to the residual projections, per GPT-2 paper
         self.apply(self._init_weights)
@@ -282,8 +459,7 @@ class GPTSoftmax(nn.Module):
         if targets is not None:
             # Masking input tokens
             targets_masked = targets.clone()
-            if self.mask_input_loss:
-                targets_masked[:, :prompt_len - 1] = -1
+            targets_masked[:, :prompt_len - 1] = -1
             loss = F.cross_entropy(
                 logits.reshape(-1, logits.size(-1)),
                 targets_masked.reshape(-1),
