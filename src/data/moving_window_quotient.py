6fc4c0ba94fbb7aa988f76d9b21a4f7fb321b80a
Jet Yue <jetyue04@gmail.com>
2026-01-23 00:47:12 -0800

    mwq

--- src/data/moving_window_quotient.py
+++ src/data/moving_window_quotient.py
@@ -14,24 +14,28 @@ class MovingWindowQuotient(BaseDataTask):
 
     @torch.no_grad()
     def sample(self, num_samples, num_tokens):
-        random_ints = torch.randint(
-            low=self.min_num, high=self.max_num + 1, size=(num_samples, num_tokens)
-        ).to(self.device)
+        # (B, T)
+        x = torch.randint(
+            low=self.min_num,
+            high=self.max_num + 1,
+            size=(num_samples, num_tokens),
+            device=self.device,
+        )
 
-        # random_ints_np = random_ints.detach().cpu().numpy()
-        # convolution = torch.stack(
-        #     [
-        #         torch.from_numpy(
-        #             np.convolve(
-        #                 random_ints_np[i],
-        #                 np.ones(self.k),
-        #                 mode="valid",
-        #             )
-        #         )
-        #         for i in range(random_ints.shape[0])
-        #     ]
-        # )
+        # Output container
+        out = torch.zeros_like(x)
 
-        # moving_sum = random_ints.clone().detach()
-        # moving_sum[:, self.k - 1 :] = convolution
-        # return moving_sum
+        # Outgoing and incoming elements
+        x_out = x[:, :-self.k]          # x_i
+        x_in  = x[:, self.k:]           # x_{i+k}
+
+        # Modular inverse of outgoing
+        x_out_inv = torch.pow(x_out, self.p - 2) % self.p
+
+        # Quotient
+        q = (x_in * x_out_inv) % self.p
+
+        # Align like moving window ops
+        out[:, self.k:] = q
+
+        return out
